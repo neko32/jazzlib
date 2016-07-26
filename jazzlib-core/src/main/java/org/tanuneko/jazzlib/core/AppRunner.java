a52121e4ef8d49cf9c6c8222bb5aa3a1c396fa1c
neko32 <neko64@outlook.jp>
2016-07-26 19:56:07 -0400

    initial commit

---
+++ jazzlib-core/src/main/java/org/tanuneko/jazzlib/core/AppRunner.java
@@ -0,0 +1,16 @@
+package org.tanuneko.jazzlib.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by neko32 on 2016/07/26.
+ */
+public class AppRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppRunner.class);
+    public static void main(String[] args) {
+        // startup
+        LOGGER.info("JazzLib starting up..");
+    }
+}
